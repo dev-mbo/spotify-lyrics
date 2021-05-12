0d578e486ec9b895138eee3b5dea8a47163c585c
Martin Born <mbo123@posteo.de>
2021-05-12 15:36:57 +0200

    create static method to fetch lyrics

--- app/main/models.py
+++ app/main/models.py
@@ -14,13 +14,28 @@ class LyricsAPI:
 
     def __init__(self):
         pass
+    
+    @staticmethod
+    def get_lyrics_for_current_playing_song(current_playing):
+        """
+        get the lyrics for the current playing song on spotify
+        current_playing: the json response for the current playing song
+        """
+        try:
+            artist = current_playing['item']['artists'][0]['name']
+            title = current_playing['item']['name']
+            return LyricsAPI.get_lyrics(artist, title)
+        except KeyError as err:
+            raise KeyError(err)
+        except Exception as err:
+            raise Exception(err)
 
-
-    def get_lyrics(self, artist, title):
+    @staticmethod
+    def get_lyrics(artist, title):
         artist = urllib.parse.quote(artist)
         title = urllib.parse.quote(title)
 
-        res = requests.get(self.lyrics_url + artist + "/" + title)
+        res = requests.get(LyricsAPI.lyrics_url + artist + "/" + title)
 
         if res.status_code != 200:
             raise Exception("error fetching lyrics")
--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,8 @@ from flask import (
     g,
     redirect,
     request,
-    render_template
+    render_template,
+    url_for
 )
 from app.main.models import LyricsAPI
 
@@ -16,10 +17,7 @@ def home():
     if 'access_token' in session:
         try:
             currently_playing = g.sp.get_currently_playing_song()
-            artist = currently_playing['item']['artists'][0]['name']
-            title = currently_playing['item']['name']
-            lyrics_api = LyricsAPI()
-            lyrics = lyrics_api.get_lyrics(artist, title)
+            lyrics = LyricsAPI.get_lyrics_for_current_playing_song(currently_playing)
             return render_template('index.html', currently_playing=currently_playing, lyrics=lyrics)
         except KeyError as err:
             return render_template('error.html', error="artist or title could not be accessed")
@@ -29,8 +27,7 @@ def home():
             ret = g.sp.refresh_access_token()
             if 'access_token' in ret:
                 session['access_token'] = ret['access_token']
-
-            return redirect("/", 302)
+            return redirect(url_for("main.home"), 302)
     else:
         res = g.sp.authorize()
         return redirect(res.url, 302)
@@ -45,6 +42,6 @@ def callback():
         if 'access_token' in ret:
             session['access_token'] = ret['access_token']
             session['refresh_token'] = ret['refresh_token']
-            return redirect("/", 302)
+            return redirect(url_for("main.home"), 302)
     except Exception as err:
         return render_template('error.html', error=err)
